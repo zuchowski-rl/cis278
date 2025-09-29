24506695ea7a7a39b710a975afb00f4fb44415b5
Robert <zuchowski-rl@users.noreply.github.com>
2025-09-28 23:00:42 -0700

    update module05 assignment part 2

--- module05/computer_assisted_instruction.cpp
+++ module05/computer_assisted_instruction.cpp
@@ -1,5 +1,6 @@
 // computer assisted instruction command line app
 // generates random multiplication questions for one digit integers
+// provides varied feedback to reduce student fatigue
 
 #include <cstdlib>  // for rand() and srand()
 #include <ctime>    // for time to use as random number generation seed
@@ -16,6 +17,52 @@ void generate_question(int &factor_1, int &factor_2) {
     // generates two numbers between 1 and 9, updates references
     factor_1 = SHIFTING_VALUE + rand() % SCALING_FACTOR;
     factor_2 = SHIFTING_VALUE + rand() % SCALING_FACTOR;
+}
+
+// function to display a random response to a correct answer
+string generate_correct_response() {
+
+    // constants to control range of random numbers
+    const int SHIFTING_VALUE{1};
+    const int SCALING_FACTOR{4};
+    // generates a choice between 1 and 4
+    int choice = SHIFTING_VALUE + rand() % SCALING_FACTOR;
+
+    switch (choice) {
+        case 1:
+            return "Very good!";
+        case 2:
+            return "Excellent!";
+        case 3:
+            return "Nice work!";
+        case 4:
+            return "Keep up the good work!";
+        default:
+            return "";
+    }
+}
+
+// function to display a random response to an incorrect answer
+string generate_incorrect_response() {
+
+    // constants to control range of random numbers
+    const int SHIFTING_VALUE{1};
+    const int SCALING_FACTOR{4};
+    // generates a choice between 1 and 4
+    int choice = SHIFTING_VALUE + rand() % SCALING_FACTOR;
+
+    switch (choice) {
+        case 1:
+            return "No. Please try again.";
+        case 2:
+            return "Wrong. Try once more.";
+        case 3:
+            return "Don't give up!";
+        case 4:
+            return "No. Keep trying.";
+        default:
+            return "";
+    }
 }
 
 // function to generate and prompt each question
@@ -35,10 +82,10 @@ bool prompt_question(int factor_1, int factor_2) {
         }
 
         if (answer == factor_1 * factor_2) {
-            cout << "Very Good!" << endl << endl;
-            return true; // correct answer
+            cout << generate_correct_response() << endl;
+            return true;
         } else {
-            cout << "No. Please try again." << endl;
+            cout << generate_incorrect_response() << endl;
         }
     }
 }
