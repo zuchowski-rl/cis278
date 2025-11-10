28c5131d5ca181eee94d3869876b8ea875b9bd9e
Robert <zuchowski-rl@users.noreply.github.com>
2025-11-09 22:55:04 -0700

    update for module 11 to demo polymorphism

--- module10-11/main.cpp
+++ module10-11/main.cpp
@@ -1,11 +1,13 @@
-// driver program to demo the Package class and its derived classes
+// driver program to demo the polymorphic use of functions of the Package class
 
 // include package class headers
 #include "OvernightPackage.h"
 #include "Package.h"
 #include "TwoDayPackage.h"
 
+#include <iomanip>  // output stream manipulation
 #include <iostream> // access to output stream
+#include <vector>   // access to vector class
 
 int main() {
 
@@ -23,42 +25,68 @@ int main() {
     const char *recipient_zip = "49007";
 
     // constants for weight and prices
-    constexpr double package_weight{10.5};
+    constexpr double package_1_weight{10.5};
+    constexpr double package_2_weight{12.75};
+    constexpr double package_3_weight{9.2};
+    constexpr double package_4_weight{20.5};
+
     constexpr double cost_per_oz(0.70);
     constexpr double flat_fee{6.99};
     constexpr double extra_fee{1.75};
 
-    // create some packages
-    // standard package
-    Package standard_pkg(sender_name, sender_address, sender_city, sender_state,
-                         sender_zip, recipient_name, recipient_address, recipient_city,
-                         recipient_state, recipient_zip, package_weight, cost_per_oz);
+    // create some packages of both types, rotating sender and recipient
+    TwoDayPackage pkg_1(sender_name, sender_address, sender_city, sender_state,
+                        sender_zip, recipient_name, recipient_address, recipient_city,
+                        recipient_state, recipient_zip, package_1_weight, cost_per_oz,
+                        flat_fee);
 
-    // two day package
-    TwoDayPackage two_day_pkg(sender_name, sender_address, sender_city, sender_state,
-                              sender_zip, recipient_name, recipient_address,
-                              recipient_city, recipient_state, recipient_zip,
-                              package_weight, cost_per_oz, flat_fee);
+    OvernightPackage pkg_2(sender_name, sender_address, sender_city, sender_state,
+                           sender_zip, recipient_name, recipient_address, recipient_city,
+                           recipient_state, recipient_zip, package_2_weight, cost_per_oz,
+                           extra_fee);
 
-    // overnight package
-    OvernightPackage overnight_pkg(sender_name, sender_address, sender_city, sender_state,
-                                   sender_zip, recipient_name, recipient_address,
-                                   recipient_city, recipient_state, recipient_zip,
-                                   package_weight, cost_per_oz, extra_fee);
+    TwoDayPackage pkg_3(recipient_name, recipient_address, recipient_city,
+                        recipient_state, recipient_zip, sender_name, sender_address,
+                        sender_city, sender_state, sender_zip, package_3_weight,
+                        cost_per_oz, flat_fee);
 
-    // display package shipping info and prices
-    std::cout << "Standard Package:" << std::endl;
-    std::cout << "---------------------------------------\n";
-    standard_pkg.print_details();
-    std::cout << std::endl;
+    OvernightPackage pkg_4(recipient_name, recipient_address, recipient_city,
+                           recipient_state, recipient_zip, sender_name, sender_address,
+                           sender_city, sender_state, sender_zip, package_4_weight,
+                           cost_per_oz, extra_fee);
 
-    std::cout << "Two Day Package:" << std::endl;
-    std::cout << "----------------------------------------\n";
-    two_day_pkg.print_details();
-    std::cout << std::endl;
+    // store all package pointers in a vector of base class pointers
+    std::vector<Package *> packages{&pkg_1, &pkg_2, &pkg_3, &pkg_4};
 
-    std::cout << "Overnight Package:" << std::endl;
-    std::cout << "----------------------------------------\n";
-    overnight_pkg.print_details();
-    std::cout << std::endl;
+    // variable to hold running total
+    double total_cost{0.0};
+
+    // use std::fixed and std::setprecision for money formatting
+    std::cout << std::fixed << std::setprecision(2);
+
+    // print info for each package
+    for (const Package *pkg : packages) {
+        std::cout << "---------------------------------------\n";
+        std::cout << "Sender:\n  ";
+        std::cout << pkg->get_sender_name() << "\n  " << pkg->get_sender_address()
+                  << "\n  " << pkg->get_sender_city() << ", " << pkg->get_sender_state()
+                  << " " << pkg->get_sender_zip() << "\n\n";
+
+        std::cout << "Recipient:\n  ";
+        std::cout << pkg->get_recipient_name() << "\n  " << pkg->get_recipient_address()
+                  << "\n  " << pkg->get_recipient_city() << ", "
+                  << pkg->get_recipient_state() << " " << pkg->get_recipient_zip()
+                  << "\n";
+
+        // polymorphic call to calculate_cost, uses overidden function in derived classes
+        double cost = pkg->calculate_cost();
+        std::cout << "\nShipping cost: $" << cost << "\n";
+        std::cout << "---------------------------------------\n";
+
+        // add package cost to total_cost
+        total_cost += cost;
+    }
+
+    // display grand total for all packages
+    std::cout << "\nTotal Shipping cost: $" << total_cost << std::endl;
 }
